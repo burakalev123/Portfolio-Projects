f97aa402103df3e0eab51294dfbd5db35694dd4d
burakalev123 <burakalev123@hotmail.com>
2021-08-10 14:39:53 +0200

    Create Procedure
    Creation of Stored Procedure Script

---
+++ INSERT DATA Script.sql
@@ -0,0 +1,11 @@
+USE YourDB
+
+INSERT INTO dbo.Products (ProductID, ProductName, Price, ProductDescription)
+
+VALUES
+
+(1, 'Clamp', 12.48, 'Workbench clamp'),
+(2, 'Screwdriver', 7.99, 'Flat head'),
+(3, 'Tire Bar', 10.99, 'Tool for changing tires')
+
+GO
