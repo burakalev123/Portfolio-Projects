55b7c7bb95ee22397c2820450432866e9b461028
burakalev123 <burakalev123@hotmail.com>
2021-10-15 16:42:28 +0200

    Update SQL Scripting.sql

--- SQL Scripting.sql
+++ SQL Scripting.sql
@@ -12,3 +12,79 @@
 -- Supports parameters
 -- Flow control logic (IF-THEN-ELSE) Loops(WHILE,FOR)
 -- Stored Procedure can return multiple results
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-- Scalar User-Defined Functions (SUDF)
+
+-- Only expressions are allowed not SELECT statements.
+
+SELECT CURRENT_DATE FROM DUMMY;  --SAP Provided Formula in use
+
+SELECT UPPER(Title), MRP FROM "T4H"."Books"; -- SAP Provided formula in use
+
+SELECT UPPER(Title), MRP, (MRP - (MRP * (20/200))) as FINAL_PRICE FROM "T4H"."Books"; -- SAP Provided and Custom Formula
+
+-- HANA DataBase Explorer
+CREATE FUNCTION FPRICE (IP DECIMAL(10,2)) -- IP variable parameter
+RETURNS RESULT DECIMAL(10,2)
+LANGUAGE SQLSCRIPT
+SQL SECURITY INVOKER AS 
+BEGIN
+RESULT:= IP - (:IP * (20/200));
+END;
+
+-- SAP Business Application Studio
+FUNCTION "FPRICE2"(IN IP DECIMAL(10, 2))
+   	RETURNS RESULT DECIMAL(10, 2)
+    LANGUAGE SQLSCRIPT
+    SQL SECURITY INVOKER AS
+BEGIN
+    /*****************************
+        Write your function logic
+    ****************************/
+    	RESULT = :IP - ( :IP * ( 20 / 200 ) );
+END;
+
+SELECT 
+	UPPER(Title),
+	MRP,
+	FPRICE(MRP) AS FINAL_PRICE
+FROM "T4H"."Books";
+
+-- Exception Handling
+FUNCTION "FPRICE2"(IN IP1 DECIMAL(10, 2), IN IP2 DECIMAL(2, 2)) 
+  RETURNS RESULT DECIMAL(10, 2) 
+  LANGUAGE SQLSCRIPT 
+  SQL SECURITY INVOKER AS 
+BEGIN
+  DECLARE INVALID_DISCOUNT CONDITION FOR SQL_ERROR_CODE 10001; -- 10001 to 99999 custom error codes, below 10000 cannot be used
+  IF :IP2 >= 0 THEN RESULT = :IP1 - (:IP1 * (IP2 / 200));
+  ELSE SIGNAL INVALID_DISCOUNT
+  SET MESSAGE_TEXT = 'Discount Value ' || :IP2 || ' must be greater than zero(0)';
+END IF;
+END;
+
+-- Factorial Function
+FUNCTION "FACTORIAL"(I INT) 
+  RETURNS F INT 
+  LANGUAGE SQLSCRIPT 
+  SQL SECURITY INVOKER AS 
+BEGIN
+  DECLARE X INT = 2;
+  F = 1;
+  WHILE X <= :I DO F = :F * :X;
+  X = :X + 1;
+END WHILE;
+END;
+
+-- Summary of Scalar UDF's
+-- 1. You can use more than one Input Parameter
+-- 2. Returns Scalar Values
+-- 3. Body can contain only expressions and imperative logic
+-- 4. You can't use SQL statements in the body.
+-- 5. You can use functions in SELECT, WHERE, GROUP BY.
+-- 6. You can call another UDF's or stored proc.
+-- 7. Most simple DB  object in SQL Scripting
+
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
