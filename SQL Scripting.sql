f5048725356bbce4a6bff50992c0b949ed7c17b3
Burak Alev <burakalev123@hotmail.com>
2021-10-17 17:14:17 +0200

    Update SQL Scripting.sql

--- SQL Scripting.sql
+++ SQL Scripting.sql
@@ -1,7 +1,7 @@
 /* What is SQL Scripting */
 -- It is the language to write stored Procedures and user-defined functions in SAP HANA
 -- It is and extension of ANSI SQL
-
+--
 /* Goal of SQL Scripting */
 -- Eliminate data transfer between database & application layers (PUSHDOWN)
 -- Calculations are executed at database layer to get benefits of HANA Database - like column operations, parallel processing of queries etc.
