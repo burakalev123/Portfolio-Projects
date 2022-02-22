905c0d6e283e57c15ef481f09419d7b50558deec
Burak Alev <65728792+burakalev123@users.noreply.github.com>
2022-02-22 18:42:11 +0100

    TimeTableSet_Code_Init

---
+++ SAC Analytical App Code Examples/TimeTableSet.js
@@ -0,0 +1,30 @@
+switch(this.getSelectedKey()) {
+	case "Q":
+	ThisApp.SetTablesTimeQ(
+				dVersion_currentMOR.properties.PLAN_START,
+				dVersion_currentMOR.properties.PLAN_END,
+				1,
+    			[tblServicePLbefore,tblServicePLafter,tblInstallPLbefore,tblInstallPLafter,tblUpgradePLbefore,tblUpgradePLafter]);
+	break;
+	case "H":
+	ThisApp.SetTablesTimeH(
+				dVersion_currentMOR.properties.PLAN_START,
+				dVersion_currentMOR.properties.PLAN_END,
+				1,
+				[tblServicePLbefore,tblServicePLafter,tblInstallPLbefore,tblInstallPLafter,tblUpgradePLbefore,tblUpgradePLafter]);
+	break;
+	case "Y":
+	ThisApp.SetTablesTimeY(
+				dVersion_currentMOR.properties.PLAN_START,
+				dVersion_currentMOR.properties.PLAN_END,
+				0,
+				[tblServicePLbefore,tblServicePLafter,tblInstallPLbefore,tblInstallPLafter,tblUpgradePLbefore,tblUpgradePLafter]);
+	break;
+			case "M":
+	ThisApp.SetTablesTimeM(
+				dVersion_currentMOR.properties.PLAN_START,
+				dVersion_currentMOR.properties.PLAN_END,
+				1,
+				[tblServicePLbefore,tblServicePLafter,tblInstallPLbefore,tblInstallPLafter,tblUpgradePLbefore,tblUpgradePLafter]);
+	break;
+}
