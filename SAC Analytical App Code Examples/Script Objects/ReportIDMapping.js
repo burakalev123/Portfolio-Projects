7a62dc6202552b0d8fcd3d1d44b6d84304dd1d93
Burak Alev <burakalev123@hotmail.com>
2022-06-13 13:08:40 +0200

    Create ReportIDMapping.js

---
+++ SAC Analytical App Code Examples/Script Objects/ReportIDMapping.js
@@ -0,0 +1,14 @@
+//function ReportIDMapping(ButtonCall: String): void
+//function maps the story, model and page ID based on the button name
+switch(ButtonCall)
+	{
+		case "Resource Planning":
+				//StoryID = <StoryID>;			
+				//ModelID = <ModelID>;
+				//PageID = <PageID>;				
+				break;
+		case "Simulate Depreciation":
+			//<<add your parameters here>>
+	}
+	
+Utils.openReceiverApp(StoryID, ModelID, PageID);
