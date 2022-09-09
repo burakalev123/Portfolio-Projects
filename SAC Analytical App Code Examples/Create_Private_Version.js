cc5ef06229fcf4b739605796c9c29007e03168c5
Burak Alev <65728792+burakalev123@users.noreply.github.com>
2022-09-09 14:18:33 +0200

    commit

--- SAC Analytical App Code Examples/Create_Private_Version.js
+++ SAC Analytical App Code Examples/Create_Private_Version.js
@@ -3,3 +3,6 @@ var publicVersion = Table_1.getPlanning().getPublicVersion("Actual");
 
 var privateVersion = publicVersion.copy("Private_Forecast_10", PlanningCopyOption.AllData, PlanningCategory.Forecast);
 console.log(privateVersion);
+
+//Publish private version as public with a new name
+var test = Table_1.getPlanning().getPrivateVersion("Private_Forecast_10").publishAs("Public_Forecast_10",PlanningCategory.Forecast);
