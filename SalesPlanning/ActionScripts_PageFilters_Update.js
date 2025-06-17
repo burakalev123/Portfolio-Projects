bf0520d4cd8b4b29c30faf6335106a10c8da6bf4
Burak Alev <65728792+burakalev123@users.noreply.github.com>
2025-06-17 22:11:43 +0200

    ads

---
+++ SalesPlanning/ActionScripts_PageFilters_Update.js
@@ -0,0 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Retrieve Company Code
+var area_f = InputControl_Area.getInputControlDataSource().getActiveSelectedMembers();
+pageFilter_Area = area_f[0];
+
+//-------------------------------------------------------------------------------------------------------
+// Retrieve Version
+pageFilter_Version = "Forecast";
